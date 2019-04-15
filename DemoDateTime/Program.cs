08230d08000c694fb4aba6265c00feff2437216e
JPetta <jovipetraratulangi@gmail.com>
2019-04-15 10:50:54 +0700

    first commit

---
+++ DemoDateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DemoDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime();
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            var tommorow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd"));
+        }
+    }
+}
